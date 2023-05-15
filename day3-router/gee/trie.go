ecaafc054b647835b85f986af481521527133c4a
weizz <weizz@ziroom.com>
2023-05-15 18:28:42 +0800

    fix

--- day3-router/gee/trie.go
+++ day3-router/gee/trie.go
@@ -36,7 +36,7 @@ func (n *node) insert(pattern string, parts []string, height int) {
 	part := parts[height]
 	child := n.matchChild(part)
 	if child == nil {
-		child := &node{part: part, isWild: part[0] == ':' || part[0] == '*'}
+		child = &node{part: part, isWild: part[0] == ':' || part[0] == '*'}
 		n.children = append(n.children, child)
 	}
 	child.insert(pattern, parts, height+1)
